966c14c2f56764370a0000f9c15192cef083083c
SuperSonic <supersonic@livemail.tw>
2019-11-24 05:15:18 +0000

    Update

---
+++ main.go
@@ -0,0 +1,15 @@
+/*
+StarStart! - Spider
+	
+LICENSE: AGPL 3.0
+Copyright(c) 2019 Star Inc. All Rights Reserved.
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Print("StarStart! Spider - Star Inc.")
+}
