f3102cceb2e4337ec0a63b7223f807a7f03c41a2
SuperSonic <supersonic@livemail.tw>
2021-02-08 18:03:22 +0800

    Using go mod for managing dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,8 +17,8 @@ else
     config_root="$HOME/.config/butterfly"
 fi
 
-configfile="config.json"
-configfile_sample="config.sample.json"
+config_file="config.json"
+config_file_sample="config.sample.json"
 
 echo ""
 echo "> Welcome to help us improving Butterfly"
@@ -27,22 +27,16 @@ echo "https://github.com/star-inc/butterfly"
 
 echo ""
 echo "Installing Go Packages"
-go get -u github.com/grokify/html-strip-tags-go
-go get -u github.com/star-inc/butterfly-solr/solr
-go get -u github.com/gocolly/colly/...
-go get -u github.com/velebak/colly-sqlite3-storage/colly/sqlite3
-go get -u github.com/PuerkitoBio/goquery
-go get -u github.com/temoto/robotstxt
+go mod download
 
-
-if [ ! -d $config_root ]; then
+if [ ! -d "$config_root" ]; then
     echo "Creating directory \`$config_root\`"
-    mkdir -p $config_root
+    mkdir -p "$config_root"
 fi
 
-if [ ! -f $configfile ]; then
-    echo "Coping \`$configfile_sample\` to \`$config_root/$configfile\`"
-    cat $configfile_sample > $config_root/$configfile
+if [ ! -f $config_file ]; then
+    echo "Coping \`$config_file_sample\` to \`$config_root/$config_file\`"
+    cat "$config_file_sample" > "$config_root/$config_file"
 fi
 
 echo ""
