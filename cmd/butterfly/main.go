55c6c5f357b0a6d523abf2d0587fb4a6f82a73ea
SuperSonic <supersonic@livemail.tw>
2021-11-14 02:30:32 +0800

    Preparing v2

--- cmd/butterfly/main.go
+++ cmd/butterfly/main.go
@@ -1,11 +1,7 @@
-/*
-Butterfly
-===
-The web crawler base on Apache Solr for StarStart!.
+// Butterfly - The web crawler base on Apache Solr for StarStart!
+// Copyright(c) 2020 Star Inc. All Rights Reserved.
+// The software licensed under Mozilla Public License Version 2.0
 
-Copyright(c) 2020 Star Inc. All Rights Reserved.
-The software licensed under Mozilla Public License Version 2.0
-*/
 package main
 
 import (
