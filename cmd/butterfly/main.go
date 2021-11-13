091c428b3975a6582a66b0e6d8755aecce3ce979
SuperSonic <supersonic@livemail.tw>
2021-11-14 02:24:41 +0800

    Reset

--- cmd/butterfly/main.go
+++ cmd/butterfly/main.go
@@ -11,9 +11,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"sync"
-
-	butterfly "github.com/star-inc/butterfly/internal"
 )
 
 var (
@@ -30,37 +27,13 @@ func init() {
 func usage() {
 	fmt.Println("Butterfly - The web crawler base on Apache Solr for StarStart!")
 	fmt.Println("(c) 2020 Star Inc. https://starinc.xyz")
-	fmt.Print("\nOptional argument:\n\n")
 }
 
 func main() {
-	if addSiteValue != "" {
-		butterfly.AddSite(addSiteValue)
-	}
-
-	if deleteSiteValue != "" {
-		butterfly.DeleteSite(deleteSiteValue)
-	}
-
 	switch flag.Arg(0) {
 	case "start":
-		taskList := new(sync.WaitGroup)
-		taskList.Add(len(butterfly.SiteList))
-		for _, siteURI := range butterfly.SiteList {
-			go func(siteURI string, taskList *sync.WaitGroup) {
-				client := butterfly.NewBody()
-				client.Fetch(siteURI)
-				taskList.Done()
-			}(siteURI, taskList)
-		}
-		taskList.Wait()
-		break
 	case "list":
-		butterfly.ShowSiteList()
-		break
 	case "config":
-		butterfly.EditConfigWithTextEditor()
-		break
 	default:
 		usage()
 	}
