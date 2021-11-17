ea8874f8fdf8784f59c59edc1801e92df8a1023b
SuperSonic <supersonic@livemail.tw>
2021-11-17 21:59:20 +0800

    Update httpClient

--- pkgs/http/client_test.go
+++ pkgs/http/client_test.go
@@ -6,8 +6,10 @@ package http
 
 import "testing"
 
+const testEndPoint = "https://reqbin.com"
+
 func Test_HttpGet(t *testing.T) {
-	client := NewHttpClient("https://reqbin.com")
+	client := NewHttpClient(testEndPoint)
 	if status, response := client.GET("/echo/get/json"); status == 200 {
 		t.Logf("Success: %s", response)
 	} else {
@@ -16,7 +18,7 @@ func Test_HttpGet(t *testing.T) {
 }
 
 func Test_HttpPost(t *testing.T) {
-	client := NewHttpClient("https://reqbin.com")
+	client := NewHttpClient(testEndPoint)
 	if status, response := client.POST("/echo/post/json", nil); status == 200 {
 		t.Logf("Success: %s", response)
 	} else {
@@ -25,7 +27,7 @@ func Test_HttpPost(t *testing.T) {
 }
 
 func Test_HttpPut(t *testing.T) {
-	client := NewHttpClient("https://reqbin.com")
+	client := NewHttpClient(testEndPoint)
 	if status, response := client.PUT("/echo/put/json", nil); status == 200 {
 		t.Logf("Success: %s", response)
 	} else {
@@ -34,7 +36,7 @@ func Test_HttpPut(t *testing.T) {
 }
 
 func Test_HttpDelete(t *testing.T) {
-	client := NewHttpClient("https://reqbin.com")
+	client := NewHttpClient(testEndPoint)
 	if status, response := client.DELETE("/echo/delete/json", nil); status == 200 {
 		t.Logf("Success: %s", response)
 	} else {
@@ -43,7 +45,7 @@ func Test_HttpDelete(t *testing.T) {
 }
 
 func Test_HttpPatch(t *testing.T) {
-	client := NewHttpClient("https://reqbin.com")
+	client := NewHttpClient(testEndPoint)
 	if status, response := client.PATCH("/echo/patch/json", nil); status == 200 {
 		t.Logf("Success: %s", response)
 	} else {
